d9e3633f8782de2fafad00082adae369a5e4e146
Errol Cleary <errol.cleary@gmail.com>
2021-01-06 15:45:57 -0800

    Finally get Babel working

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,2 +1,5 @@
+// import React from '../node_modules/react/index.js';
+// const Component = React.Component;
+// import ReactDOM from '../node_modules/react-dom/index.js';
 
-
+ReactDOM.render(<h1>Hello, World</h1>, document.getElementById('app'));
