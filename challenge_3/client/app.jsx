681f36ec71350183bbc02c56ce7f0998fa9a1084
Errol Cleary <errol.cleary@gmail.com>
2021-01-06 16:07:49 -0800

    Create empty component for each page

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -2,4 +2,76 @@
 // const Component = React.Component;
 // import ReactDOM from '../node_modules/react-dom/index.js';
 
-ReactDOM.render(<h1>Hello, World</h1>, document.getElementById('app'));
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      displayPage: 'Home'
+    };
+  };
+
+  render() {
+    return (
+      <div>
+        <Home />
+        <AccountForm />
+        <ShippingForm />
+        <PaymentForm />
+        <Confirmation />
+      </div>
+    );
+  };
+}
+
+const Home = (props) => {
+  return (
+    <div>Hello, Cheeseplanet</div>
+  );
+};
+
+class AccountForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  };
+
+  render() {
+    return (
+      <div></div>
+    );
+  };
+}
+
+class ShippingForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  };
+
+  render() {
+    return (
+      <div></div>
+    );
+  };
+}
+
+class PaymentForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  };
+
+  render() {
+    return (
+      <div></div>
+    );
+  };
+}
+
+const Confirmation = (props) => {
+  return (
+    <div></div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById('app'));
